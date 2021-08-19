f56d49cde772bbc3fe9e205693fbe2c5612eae49
eliascmaciiel <eliascmaciiel@gmail.com>
2021-08-19 17:17:22 -0300

    Update 10.py

--- 10.py
+++ 10.py
@@ -1,4 +1,4 @@
-qnt = float(input("Olá, desculpe a indescência, mas quantos R$ você tem na carteira? "))
+qnt = float(input("Olá, desculpe a incoveniência, mas quantos R$ você tem na carteira? "))
 
 dolar = qnt * 5.41
 
